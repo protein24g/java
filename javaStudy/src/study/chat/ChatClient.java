0dcec527eeb2c3b100c185804e71cfe8cabe894d
protein24g <dns6574@naver.com>
2023-12-16 02:33:14 +0900

    commit

---
+++ javaStudy/src/study/chat/ChatClient.java
@@ -0,0 +1,4 @@
+package toyProject.chat;
+
+public class ChatClient {
+}
---
+++ javaStudy/src/study/chat/ChatServer.java
@@ -0,0 +1,34 @@
+package study.chat;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+
+public class ChatServer {
+    public static void main(String[] args) throws Exception{
+        ServerSocket serverSocket = new ServerSocket(9999);
+        List<PrintWriter> outList = Collections.synchronizedList(new ArrayList<>());
+        while(true){
+            Socket socket = serverSocket.accept();
+            ChatThread chatThread = new ChatThread(socket, outList);
+            chatThread.start();
+
+        }
+
+    }
+}
+
+class ChatThread extends Thread{
+    private Socket socket;
+    private List<PrintWriter> outList;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ChatThread(Socket socket, List<PrintWriter> outList){
+        this.socket = socket;
+        this.outList = outList;
+    }
+
+    public void run(){
+
+    }
+}
