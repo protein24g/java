abd9d6d80f3d12988ec265ec9428294c86520c6d
protein24g <dns6574@naver.com>
2023-12-18 11:56:35 +0900

    commit

--- javaStudy/src/study/chat/Example02/Client.java
+++ javaStudy/src/study/chat/Example02/Client.java
@@ -1,4 +1,27 @@
 package study.chat.Example02;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client {
+    public static void main(String[] args) {
+        BufferedWriter out = null;
+        Scanner scanner = new Scanner(System.in);
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        try{
+            socket = new Socket("localhost", 9999);
+            Thread receiver = new Thread(new Receiver(socket));
+            receiver.start();
+
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            while(true){
+                String string = scanner.nextLine();
+                if(string == null) break;
+                out.write(string + "\n");
+                out.flush();
+            }
+        }catch (Exception e){}
+    }
 }
--- javaStudy/src/study/chat/Example02/Receiver.java
+++ javaStudy/src/study/chat/Example02/Receiver.java
@@ -1,4 +1,20 @@
 package study.chat.Example02;
 
-public class Receiver {
+import java.io.*;
+import java.net.*;
+
+public class Receiver implements Runnable{
+    Socket socket;
+    public Receiver(Socket socket){this.socket = socket;}
+
+    public void run(){
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while(true){
+                String string = in.readLine();
+                if(string == null) break;
+                System.out.println(string);
+            }
+        }catch (Exception e){}
+    }
 }
