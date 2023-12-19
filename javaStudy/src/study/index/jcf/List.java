cc23c4785239130edab62d1229eaa0b414267196
protein24g <dns6574@naver.com>
2023-12-20 02:37:23 +0900

    commit

--- javaStudy/src/study/index/jcf/List.java
+++ javaStudy/src/study/index/jcf/List.java
@@ -1,4 +1,19 @@
 package study.index.jcf;
 
+import java.util.LinkedList;
+
 public class List {
+    public static void main(String[] args) {
+        LinkedList<String> stack = new LinkedList<>();
+        stack.push("가");
+        stack.push("나");
+        stack.push("다");
+        System.out.println("현재 스택 : " + stack);
+
+        String data = stack.pop();
+        System.out.println("꺼내온 값 : " + data);
+        System.out.println("꺼내온 값 : " + stack.pop());
+        System.out.println("현재 스택 : " + stack);
+    }
+
 }
---
+++ javaStudy/src/study/index/jcf/Queue.java
@@ -0,0 +1,7 @@
+package study.index.jcf;
+
+public class Queue {
+    public static void main(String[] args) {
+
+    }
+}
